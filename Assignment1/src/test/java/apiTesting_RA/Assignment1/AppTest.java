396b9f87b09ca9bc3e27b69cf62babcc45c925d5
Rizwan Ali <rizcubeservices@gmail.com>
2020-06-24 09:15:44 +0100

    created new maven project

---
+++ Assignment1/src/test/java/apiTesting_RA/Assignment1/AppTest.java
@@ -0,0 +1,20 @@
+package apiTesting_RA.Assignment1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
