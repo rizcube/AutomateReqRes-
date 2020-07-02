57f81bc16749a923ffe7f229e56c31869479c91c
Rizwan Ali <rizcubeservices@gmail.com>
2020-07-02 21:18:04 +0100

    some more progress on api 4 - LIST <RESOURCE> in ResourceTests file

--- Assignment1/src/test/java/files/ResourceTests.java
+++ Assignment1/src/test/java/files/ResourceTests.java
@@ -3,41 +3,45 @@ import static io.restassured.RestAssured.*;
 
 import java.util.List;
 
+import org.apache.http.HttpRequest;
+
 import io.restassured.parsing.Parser;
+import io.restassured.response.Response;
 
 public class ResourceTests {
 	
 	public void testAll() {
-		test_get_resourceList_returns_HTTP_200();
+		test_get_resourceList_returns_resourceList_with_HTTP_200();
 		//test_get_singleResource_notFound_returns_HTTP_200();
 		//test_get_invalidResource_returns_HTTP_404();
 		
 	}
 
-	private void test_get_resourceList_returns_HTTP_200() {
+	private void test_get_resourceList_returns_resourceList_with_HTTP_200() {
 		String[] expectedResourceNames = {"cerulean","fuchsia rose","true red","aqua sky","tigerlily","blue turquoise"};
 		
 		// List Resource getCall 4
-		System.out.println("test_get_resourceList_returns_HTTP_200() api 4 - LIST <RESOURCE> ");
+		System.out.println("test_get_resourceList_returns_resourceList_with_HTTP_200() api 4 - LIST <RESOURCE> ");
 		ListResources lr = given().expect().defaultParser(Parser.JSON)
 				.when()
 				.get("/api/unknown")
 				.then().assertThat().statusCode(200)
 				.extract().response().as(ListResources.class);
-								
-				System.out.println("List Resource getCall 4");
-				System.out.println(lr.getPage());
-				System.out.println(lr.getData().get(0).getName());
-				
-		
 				
 				int length = expectedResourceNames.length;
-				System.out.println("I am the object of resource");
+				List<files.ResourceData> ResourceData = lr.getData();
+				//System.out.println(ResourceData);
+				for (int i =0; i< length; i++) 
+				{
+					lr.getData().get(i).getName();
+					if (lr.getData().get(i).getName().equalsIgnoreCase("fuchsia rose"))
+					{
+						System.out.println("Name >  "+ResourceData.get(i).getName());
+						System.out.println("Year >  "+ResourceData.get(i).getYear());
+						System.out.println("Color>  "+ResourceData.get(i).getColor());
+						System.out.println("Pantone_value > "+ResourceData.get(i).getPantone_value());
+					}
 				
-				List<files.ResourceData> ResourceData = System.out.println(lr.getData());
-				
-				for (int i =0; i< length; i++) {
-					lr.getData().get(0).getName();
 				}
 				
 				
