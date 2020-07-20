404a236f40a6e12338d284ff6ce647f3bffcbd3c
Rizwan Ali <rizcubeservices@gmail.com>
2020-07-20 12:30:43 +0100

    changed preferences to formatSourceCode

--- Assignment1/src/test/java/files/ReUseableMethods.java
+++ Assignment1/src/test/java/files/ReUseableMethods.java
@@ -1,12 +1,11 @@
 package files;
+
 // Assignment file
 import io.restassured.path.json.JsonPath;
 
 public class ReUseableMethods {
-	public static JsonPath rawToJson (String response) 
-	{
+	public static JsonPath rawToJson(String response) {
 		JsonPath js = new JsonPath(response);
 		return js;
 	}
-	
 }
